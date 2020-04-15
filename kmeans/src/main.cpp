9a3a61be0986594ab54590ca3ef560f771da9487
Vlad <vladvan711@gmail.com>
2020-04-14 21:19:17 -0400

    kmeans folder and example opencv code

---
+++ kmeans/src/main.cpp
@@ -0,0 +1,23 @@
+#include "opencv2/opencv.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <ctime>
+
+#include "config.h"
+
+using namespace cv;
+using namespace std;
+
+
+
+int main( int argc, const char** argv ) {
+    int a = 0;
+    cout << "HI from CV hello world" << endl;
+    return 0;
+
+}
+
