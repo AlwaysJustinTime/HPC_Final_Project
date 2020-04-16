76be0052fb1f1ac19f1af30094821a5009806fea
Vlad <vladvan711@gmail.com>
2020-04-15 23:25:42 -0400

    cool python script

--- kmeans/src/main.cpp
+++ kmeans/src/main.cpp
@@ -4,12 +4,70 @@
 #include "opencv2/highgui.hpp"
 #include "opencv2/core.hpp"
 #include "opencv2/imgproc.hpp"
+#include "opencv2/ml/ml.hpp"
 #include <iostream>
+#include <string>
+#include <fstream>
 
-#define NUM_ITERATIONS 5
+#define DATA_CSV "../data/suite_160/cluster_2.csv"
 
 using namespace cv;
 using namespace std;
+
+
+// Timer function
+double CLOCK() {
+	struct timespec t;
+	clock_gettime(CLOCK_MONOTONIC,  &t);
+	return (t.tv_sec * 1000)+(t.tv_nsec*1e-6);
+}
+
+
+// to read the given file into a cv::Mat
+Mat readCSV(char* filename) {
+    ifstream inputfile (filename, ifstream::in);
+    string current_line;
+    // vector allows you to add data without knowing the exact size beforehand
+    vector< vector<double> > all_data;
+    vector<int> all_labels;
+    // Start reading lines as long as there are lines in the file
+    while(getline(inputfile, current_line)){
+        // Now inside each line we need to seperate the cols
+        vector<double> values;
+        int label;
+        stringstream temp(current_line);
+        string single_value;
+
+        int count = 0;
+        while(getline(temp,single_value,',')){
+            if (count == 2) {
+                label = atoi(single_value.c_str());
+            }
+            else {
+                // convert the string element to a integer value
+                values.push_back(atof(single_value.c_str()));
+            }
+            count += 1;
+        }
+        // add the row to the complete data vector
+        all_data.push_back(values);
+        all_labels.push_back(label);
+    }
+
+    // Now add all the data into a Mat element
+    Mat vect = Mat::zeros((int)all_data.size(), (int)all_data[0].size(), CV_32FC1);
+    // // Loop over vectors and add the data
+    for(int rows = 0; rows < (int)all_data.size(); rows++){
+        for(int cols= 0; cols< (int)all_data[0].size(); cols++){
+            vect.at<float>(rows,cols) = all_data[rows][cols]; 
+        }
+    }
+
+    // cout << "M = " << endl << " " << vect << endl;
+    return vect;
+}
+
+
 
 // static void help()
 // {
@@ -20,65 +78,35 @@ using namespace std;
 //             "./kmeans\n" << endl;
 // }
 
-int main( int /*argc*/, char** /*argv*/ )
+int main( int argc, char** argv )
 {
-    const int MAX_CLUSTERS = 5;
-    Scalar colorTab[] =
-    {
-        Scalar(0, 0, 255),
-        Scalar(0,255,0),
-        Scalar(255,100,100),
-        Scalar(255,0,255),
-        Scalar(0,255,255)
-    };
-
-    Mat img(500, 500, CV_8UC3);
-    RNG rng(12345);
-
-    for(int ii = 0; ii < NUM_ITERATIONS; ii += 1)
-    {
-        int k, clusterCount = rng.uniform(2, MAX_CLUSTERS+1);
-        int i, sampleCount = rng.uniform(1, 1001);
-        Mat points(sampleCount, 1, CV_32FC2), labels;
-
-        clusterCount = MIN(clusterCount, sampleCount);
-        std::vector<Point2f> centers;
-
-        /* generate random sample from multigaussian distribution */
-        for( k = 0; k < clusterCount; k++ )
-        {
-            Point center;
-            center.x = rng.uniform(0, img.cols);
-            center.y = rng.uniform(0, img.rows);
-            Mat pointChunk = points.rowRange(k*sampleCount/clusterCount,
-                                             k == clusterCount - 1 ? sampleCount :
-                                             (k+1)*sampleCount/clusterCount);
-            rng.fill(pointChunk, RNG::NORMAL, Scalar(center.x, center.y), Scalar(img.cols*0.05, img.rows*0.05));
-        }
-
-        randShuffle(points, 1, &rng);
-
-        double compactness = kmeans(points, clusterCount, labels,
-            TermCriteria( TermCriteria::EPS+TermCriteria::COUNT, 10, 1.0),
-               3, KMEANS_PP_CENTERS, centers);
-
-        img = Scalar::all(0);
-
-        for( i = 0; i < sampleCount; i++ )
-        {
-            int clusterIdx = labels.at<int>(i);
-            Point ipt = points.at<Point2f>(i);
-            circle( img, ipt, 2, colorTab[clusterIdx], FILLED, LINE_AA );
-        }
-        for (i = 0; i < (int)centers.size(); ++i)
-        {
-            Point2f c = centers[i];
-            circle( img, c, 40, colorTab[i], 1, LINE_AA );
-        }
-        cout << "Compactness: " << compactness << endl;
-
-        // imshow("clusters", img);
+    if(argc < 3) {
+        cout << "Please enter more arguments" << endl;
+        return -1;
     }
+
+    double start_time, end_time;
+    start_time = CLOCK();
+
+    Mat labels;
+    std::vector<Point2f> centers;
+    int i;
+    int clusterCount = atoi(argv[1]);
+    char* fname= argv[2];
+
+    Mat points = readCSV(fname);
+
+
+    double compactness = kmeans(points, clusterCount, labels,
+        TermCriteria( TermCriteria::EPS+TermCriteria::COUNT, 10, 1.0),
+            3, KMEANS_PP_CENTERS, centers);
+    cout << "Compactness: " << compactness << endl;
+
+
+
+
+    end_time = CLOCK();
+    cout << "OpemMP execution time: " << end_time - start_time << " ms" << endl;
 
     return 0;
 }
