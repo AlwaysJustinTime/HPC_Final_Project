5d7975d55df30b6745cb85482a12bc230be565e2
Justin Adams <adams.justin.p@gmail.com>
2020-04-15 22:22:41 -0400

    added graphing

--- data_generator.py
+++ data_generator.py
@@ -1,10 +1,10 @@
-DATA_PER_FILE = [10,20,30,40]
+DATA_PER_FILE = [20,40,80,160]
 TRUE_CLUSTERS_NUMBERS = [2,3,4,5]
 HEAD_FOLDER_NAME = "data"
 FOLDER_NAMES = {num: "suite_"+str(num) for num in DATA_PER_FILE}
 FILE_NAMES = {num: "cluster_"+str(num)+".csv" for num in TRUE_CLUSTERS_NUMBERS}
 CLUSTER_STD = 0.25
-GRAPH_DATA = False
+GRAPH_DATA = True
 
 from sklearn.datasets.samples_generator import make_blobs
 import matplotlib.pyplot as plt
@@ -17,13 +17,12 @@ def get_unit_circle_centers(cluster_num):
     answer = [(math.cos(increment*num),math.sin(increment*num)) for num in range(cluster_num)]
     return answer
 
-centers = {cluster_num:get_unit_circle_centers(cluster_num) for cluster_num in TRUE_CLUSTERS_NUMBERS}
-
-def graph_centers(centers):
-    for cluster_num in TRUE_CLUSTERS_NUMBERS:
-        plt.scatter(*zip(*centers[cluster_num]))
+def graph_data(data, labels, centers):
+    plt.scatter(*zip(*centers), color="red", s=20, label="Centers")
+    for i in range(len(centers)):
+        plt.scatter(data[labels == i, 0], data[labels == i, 1], s=10, label="Cluster"+str(i))
+    plt.axis([-2, 2, -2, 2])
     plt.show()
-if GRAPH_DATA: graph_centers(centers)
 
 def write_to_csv(data, labels, filepath):
     data = pd.DataFrame([[data[idx][0], data[idx][1], labels[idx]] for idx in range(len(data))])
@@ -33,6 +32,9 @@ def gen_data(number_samples, centers, filepath):
     cluster_std = [CLUSTER_STD for _ in centers]
     data, labels = make_blobs(n_samples=number_samples, cluster_std=cluster_std, centers=centers, n_features=2)
     write_to_csv(data, labels, filepath)
+    if GRAPH_DATA: graph_data(data, labels, centers)
+
+centers = {cluster_num:get_unit_circle_centers(cluster_num) for cluster_num in TRUE_CLUSTERS_NUMBERS}
 
 for num_of_data in DATA_PER_FILE:
     folder_name = os.path.join(HEAD_FOLDER_NAME, FOLDER_NAMES[num_of_data])
@@ -41,6 +43,3 @@ for num_of_data in DATA_PER_FILE:
     for cluster_num in TRUE_CLUSTERS_NUMBERS:
         filepath = os.path.join(folder_name, FILE_NAMES[cluster_num])
         gen_data(num_of_data, centers[cluster_num], filepath)
-
-# plt.scatter(X[y == 0, 0], X[y == 0, 1], color="red", s=10, label="Cluster1")
-# plt.scatter(X[y == 1, 0], X[y == 1, 1], color="blue", s=10, label="Cluster2")
